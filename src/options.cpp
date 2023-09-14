ef09df1325101b2c14b79f6f7b2b4627437ffaab
Theofilos Intzoglou <int.teo@gmail.com>
2023-09-14 13:19:18 +0300

    Reorganize program arguments to provide separate options for image and QR

--- src/options.cpp
+++ src/options.cpp
@@ -16,36 +16,53 @@ readCLIOptions(int argc, char *argv[]) {
     bool invalid_value = false;
 
     // parse options using boost::program_options
-    options_description desc("Allowed options:");
+    options_description generic("Generic");
     // clang-format off
-    desc.add_options()
+    generic.add_options()
         ("help,h", "Produce this help message")
         ("input-file,i", value<std::string>()->required(), "Input file")
-        ("stamp,s", value<std::string>(), "Image to embed")
         ("output-file,o", value<std::string>()->required(), "Output file")
-        ("side", value<int>()->default_value(0), "Side of the document: 0 center (default), 1 left, 2 right")
         ("rotate", value<int>()->default_value(0), "Assume page is rotated by 0/90/180/270 degrees")
+        ("debug", "Print extra debug messages");
+
+    options_description imgOptions("Image");
+    imgOptions.add_options()
+        ("stamp,s", value<std::string>(), "Image to embed")
+        ("img-side", value<int>()->default_value(0), "Side of the document: 0 center (default), 1 left, 2 right")
+        ("img-scale", value<float>()->default_value(1),"Scale image by a factor eg. 0.5")
+        ("img-top-margin", value<float>()->default_value(10),"Set a margin for the image placement from the top of the page")
+        ("img-side-margin", value<float>()->default_value(15),"Set a margin for the image placement from the sides of the page");
+
+    options_description qrOptions("QR");
+    qrOptions.add_options()
         ("qr", value<std::string>(), "Add QR instead of image using the specified text")
         ("link", "QR value is a URL. Add clickable link")
-        ("scale", value<float>()->default_value(1),"Scale image by a factor eg. 0.5")
-        ("top-margin", value<float>()->default_value(10),"Set a margin for the image placement from the top of the page")
-        ("side-margin", value<float>()->default_value(15),"Set a margin for the image placement from the sides of the page")
+        ("qr-side", value<int>()->default_value(0), "Side of the document: 0 center (default), 1 left, 2 right")
+        ("qr-scale", value<float>()->default_value(1),"Scale QR by a factor eg. 0.5")
+        ("qr-top-margin", value<float>()->default_value(10),"Set a margin for the QR placement from the top of the page")
+        ("qr-side-margin", value<float>()->default_value(15),"Set a margin for the QR placement from the sides of the page");
+
+    options_description textOptions("Text (only in latin alphabet)");
+    textOptions.add_options()
         ("add-text", value<std::vector<std::string>>()->multitoken(), "Add extra text to the first page. It can take multiple "
             "strings of the form '[x,y:][size:][style:]text' where x,y are the coordinates where the text will appear, size "
-            "is a float and gives the font size of the text and style can be 'i', 'b', 'bi', 'ib' for italic, bold and bold+italic")
-        ("debug", "Print extra debug messages");
+            "is a float and gives the font size of the text and style can be 'i', 'b', 'bi', 'ib' for italic, bold and bold+italic");
+
+    options_description programOptions("Valid Options");
+    programOptions.add(generic).add(imgOptions).add(qrOptions).add(textOptions);
     // clang-format on
 
     variables_map vm;
     try {
-        store(command_line_parser(argc, argv).options(desc).run(), vm);
+        store(command_line_parser(argc, argv).options(programOptions).run(),
+              vm);
         notify(vm);
     } catch (error &e) {
         if (!vm.count("help")) {
             std::cerr << e.what() << std::endl << std::endl;
         }
 
-        std::cout << desc << std::endl;
+        std::cout << programOptions << std::endl;
         exit(1);
     }
 
@@ -61,10 +78,10 @@ readCLIOptions(int argc, char *argv[]) {
         cliOption["imageFile"] = vm["stamp"].as<std::string>();
     }
 
-    if (vm.count("side")) {
+    if (vm.count("img-side")) {
         int side;
 
-        side = vm["side"].as<int>();
+        side = vm["img-side"].as<int>();
         if (side < 0 || side > 2) {
             invalid_value = true;
         }
@@ -76,7 +93,25 @@ readCLIOptions(int argc, char *argv[]) {
             exit(2);
         }
 
-        cliOption["side"] = side;
+        cliOption["img-side"] = side;
+    }
+
+    if (vm.count("qr-side")) {
+        int side;
+
+        side = vm["qr-side"].as<int>();
+        if (side < 0 || side > 2) {
+            invalid_value = true;
+        }
+
+        if (invalid_value) {
+            std::cerr << "Wrong value for option --side. Valid options are "
+                         "0, 1, 2."
+                      << std::endl;
+            exit(2);
+        }
+
+        cliOption["qr-side"] = side;
     }
 
     if (vm.count("rotate")) {
@@ -108,24 +143,43 @@ readCLIOptions(int argc, char *argv[]) {
             cliOption["link"] = std::string();
     }
 
-    float scale;
-    scale = vm["scale"].as<float>();
-    if (scale < 0) {
+    float img_scale;
+    img_scale = vm["img-scale"].as<float>();
+    if (img_scale < 0) {
+        invalid_value = true;
+    }
+
+    if (invalid_value) {
+        std::cerr << "Bad value for option img-scale. Valid options are real "
+                     "values >0"
+                  << std::endl;
+        exit(2);
+    }
+
+    cliOption["img-scale"] = img_scale;
+
+    float qr_scale;
+    qr_scale = vm["qr-scale"].as<float>();
+    if (qr_scale < 0) {
         invalid_value = true;
     }
 
     if (invalid_value) {
         std::cerr
-            << "Bad value for option scale. Valid options are real values >0"
+            << "Bad value for option qr-scale. Valid options are real values >0"
             << std::endl;
         exit(2);
     }
 
-    cliOption["scale"] = scale;
+    cliOption["qr-scale"] = qr_scale;
 
-    cliOption["top-margin"] = vm["top-margin"].as<float>();
+    cliOption["img-top-margin"] = vm["img-top-margin"].as<float>();
 
-    cliOption["side-margin"] = vm["side-margin"].as<float>();
+    cliOption["img-side-margin"] = vm["img-side-margin"].as<float>();
+
+    cliOption["qr-top-margin"] = vm["qr-top-margin"].as<float>();
+
+    cliOption["qr-side-margin"] = vm["qr-side-margin"].as<float>();
 
     cliOption["text"] = vm.count("add-text")
                             ? vm["add-text"].as<std::vector<std::string>>()
@@ -139,7 +193,7 @@ readCLIOptions(int argc, char *argv[]) {
         (!cliOption.contains("qrText") && !cliOption.contains("imageFile") &&
          !cliOption.contains("text")) ||
         vm.empty() || vm.count("help")) {
-        std::cout << desc << std::endl;
+        std::cout << programOptions << std::endl;
         exit(1);
     }
 
